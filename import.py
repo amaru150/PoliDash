a039ef9133ef08f0a9ee065faed4226be4a7c404
Amaru Lewis <4211126+amaru150@users.noreply.github.com>
2020-08-12 17:02:41 -0400

    simplified list politician function

--- import.py
+++ import.py
@@ -102,56 +102,28 @@ def state_import():
     politician = {}
     legislation = {}
 
-    def list_politicians(chamber,delta):
-        while delta > 0:
-            req = requests.get(f'http://legislation.nysenate.gov/api/3/members/{2009+delta}/{chamber}?full=true&limit=200&key={api_key}')
+    def list_politicians(chamber,delta,limit):
+        for year in range (2009,curent_year):
+            req = requests.get(f'http://legislation.nysenate.gov/api/3/members/{year}/{chamber}?full=true&limit={limit}&key={api_key}')
             resp = req.json()
-            delta -= 1
+            offsetEnd = resp['offsetEnd']
             if (resp['success'] == 1):
-                length = resp['offsetEnd']
-                count = 0
-                while  count < 50: #length
-                    memberId = resp['result']['items'][count]['memberId']
-                    if memberId not in politician or (politician[memberId] == {}):
-                        chamber = resp['result']['items'][count]['chamber']
-                        incumbent = resp['result']['items'][count]['incumbent']
-                        shortName = resp['result']['items'][count]['shortName']
-                        session_years = list(resp['result']['items'][count]['sessionShortNameMap'])
-
-                        personId = resp['result']['items'][count]['person']['personId'] 
-                        fullName = resp['result']['items'][count]['person']['fullName'] 
-                        firstName = resp['result']['items'][count]['person']['firstName'] 
-                        middleName = resp['result']['items'][count]['person']['middleName'] 
-                        lastName = resp['result']['items'][count]['person']['lastName']
-                        email = resp['result']['items'][count]['person']['email'] 
-                        prefix = resp['result']['items'][count]['person']['prefix'] 
-                        suffix = resp['result']['items'][count]['person']['suffix'] 
-                        verified = resp['result']['items'][count]['person']['verified']
-                        districtCode = resp['result']['items'][count]['districtCode']
-
-                        info = {memberId:{
-                            'chamber':chamber,
-                            'incumbent':incumbent,
-                            'shortName':shortName,
-                            'session_years':session_years,
-                            'personId':personId,
-                            'fullName':fullName,
-                            'firstName':firstName,
-                            'middleName':middleName,
-                            'lastName':lastName,
-                            'email':email,
-                            'prefix':prefix,
-                            'suffix':suffix,
-                            'verified':verified,
-                            'districtCode':districtCode
-                            }
-                        }
-                        politician.update(info)
-                    else:
-                        pass
-                    
-                    count += 1
-    
+                total_pages = math.ceil(resp['total'] / limit)
+                for page in range (1,2):
+                    req = requests.get(f'http://legislation.nysenate.gov/api/3/members/{year}/{chamber}?full=true&offset={offsetEnd+1}full=true&key={api_key}')
+                    resp = req.json()
+                    for members_ct in range(1,limit):
+                        member = {}
+                        items = resp['result']['items'][members_ct]
+                        for field in list(items):
+                            content = items.get(field)
+                            primarykey = items.get('memberId')
+                            data = unpack_nests(field,content)
+                            member.update(data)
+                            politician.update(
+                                {primarykey: member}
+                            )
+   
     list_politicians('senate')
     print(politician)
     print(list(politician[384]))
@@ -164,27 +136,26 @@ def state_import():
 
 
    def state_bills(delta,limit):
-            for year in range (2009,curent_year):
-                print(year)
-                req = requests.get(f'http://legislation.nysenate.gov/api/3/bills/{year}?limit={limit}&full=true&key={api_key}')
-                resp = req.json()
-                offsetEnd = resp['offsetEnd']
-                if (resp['success'] == 1):
-                    total_pages = math.ceil(resp['total'] / limit)
-                    for page in range (1,total_pages):
-                        req = requests.get(f'http://legislation.nysenate.gov/api/3/bills/{year}?limit={limit}&offset={offsetEnd+1}full=true&key={api_key}')
-                        resp = req.json()
-                        for bills in range (1,limit):
-                            bill = {}
-                            items = resp['result']['items'][i]
-                            for field in list(items):
-                                content = items.get(field)
-                                primarykey = items.get('basePrintNo')
-                                data = unpack_nests(field,content)
-                                bill.update(data)
-                                legislation.update(
-                                    {primarykey: bill}
-                                )
+        for year in range (2009,curent_year):
+            req = requests.get(f'http://legislation.nysenate.gov/api/3/bills/{year}?limit={limit}&full=true&key={api_key}')
+            resp = req.json()
+            offsetEnd = resp['offsetEnd']
+            if (resp['success'] == 1):
+                total_pages = math.ceil(resp['total'] / limit)
+                for page in range (1,total_pages):
+                    req = requests.get(f'http://legislation.nysenate.gov/api/3/bills/{year}?limit={limit}&offset={offsetEnd+1}full=true&key={api_key}')
+                    resp = req.json()
+                    for bills in range (1,limit):
+                        bill = {}
+                        items = resp['result']['items'][i]
+                        for field in list(items):
+                            content = items.get(field)
+                            primarykey = items.get('basePrintNo')
+                            data = unpack_nests(field,content)
+                            bill.update(data)
+                            legislation.update(
+                                {primarykey: bill}
+                            )
 
     
 
--- tests.py
+++ tests.py
@@ -38,55 +38,28 @@ def state_import():
     politician = {}
     legislation = {}
 
- def list_politicians(chamber,delta):
-        while delta > 0:
-            req = requests.get(f'http://legislation.nysenate.gov/api/3/members/{2009+delta}/{chamber}?full=true&limit=200&key={api_key}')
+    def list_politicians(chamber,delta,limit):
+        for year in range (2009,curent_year):
+            req = requests.get(f'http://legislation.nysenate.gov/api/3/members/{year}/{chamber}?full=true&limit={limit}&key={api_key}')
             resp = req.json()
-            delta -= 1
+            offsetEnd = resp['offsetEnd']
             if (resp['success'] == 1):
-                length = resp['offsetEnd']
-                count = 0
-                while  count < 50: #length
-                    memberId = resp['result']['items'][count]['memberId']
-                    if memberId not in politician or (politician[memberId] == {}):
-                        chamber = resp['result']['items'][count]['chamber']
-                        incumbent = resp['result']['items'][count]['incumbent']
-                        shortName = resp['result']['items'][count]['shortName']
-                        session_years = list(resp['result']['items'][count]['sessionShortNameMap'])
+                total_pages = math.ceil(resp['total'] / limit)
+                for page in range (1,2):
+                    req = requests.get(f'http://legislation.nysenate.gov/api/3/members/{year}/{chamber}?full=true&offset={offsetEnd+1}full=true&key={api_key}')
+                    resp = req.json()
+                    for members_ct in range(1,limit):
+                        member = {}
+                        items = resp['result']['items'][members_ct]
+                        for field in list(items):
+                            content = items.get(field)
+                            primarykey = items.get('memberId')
+                            data = unpack_nests(field,content)
+                            member.update(data)
+                            politician.update(
+                                {primarykey: member}
+                            )
 
-                        personId = resp['result']['items'][count]['person']['personId'] 
-                        fullName = resp['result']['items'][count]['person']['fullName'] 
-                        firstName = resp['result']['items'][count]['person']['firstName'] 
-                        middleName = resp['result']['items'][count]['person']['middleName'] 
-                        lastName = resp['result']['items'][count]['person']['lastName']
-                        email = resp['result']['items'][count]['person']['email'] 
-                        prefix = resp['result']['items'][count]['person']['prefix'] 
-                        suffix = resp['result']['items'][count]['person']['suffix'] 
-                        verified = resp['result']['items'][count]['person']['verified']
-                        districtCode = resp['result']['items'][count]['districtCode']
-
-                        info = {memberId:{
-                            'chamber':chamber,
-                            'incumbent':incumbent,
-                            'shortName':shortName,
-                            'session_years':session_years,
-                            'personId':personId,
-                            'fullName':fullName,
-                            'firstName':firstName,
-                            'middleName':middleName,
-                            'lastName':lastName,
-                            'email':email,
-                            'prefix':prefix,
-                            'suffix':suffix,
-                            'verified':verified,
-                            'districtCode':districtCode
-                            }
-                        }
-                        politician.update(info)
-                    else:
-                        pass
-                    
-                    count += 1
     
 
     def state_bills(delta,limit):
@@ -112,8 +85,8 @@ def state_import():
                                     {primarykey: bill}
                                 )
 
-    state_bills(delta,10)
-    print(legislation)
+    list_politicians('senate',delta,10)
+    print(politician[384])
 
     
 state_import()
